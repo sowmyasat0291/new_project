dff219c8c08211fc2a1f774664e97be80a19562a
Sowmya <sowmyam0291@gmail.com>
2024-08-29 13:33:09 +0530

    Test Commit

--- contact_form/contact/views.py
+++ contact_form/contact/views.py
@@ -1,5 +1,45 @@
-from django.shortcuts import render
+from django.core.mail import send_mail
+from django.shortcuts import render, redirect
+from django.template.loader import render_to_string
+from .forms import ContactForm
 
-# Create your views here.
 def index(request):
-    return render(request, 'contact/index.html')
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+            
+            # Render the HTML message
+            html = render_to_string('contact/emails/contactform.html', {
+                'name': name,
+                'email': email,
+                'subject': subject,
+                'message': message,
+            })
+
+            # Define the "From" email address and recipient list
+            from_email = 'your_custom_from_email@example.com'  # The desired "From" email address
+            recipient_list = ['sowmyam0291@gmail.com']
+
+            try:
+                send_mail(
+                    subject,
+                    message,
+                    from_email,
+                    recipient_list,
+                    html_message=html
+                )
+                print('The form is valid and email was sent.')
+                return redirect('index')
+            except Exception as e:
+                print(f'Error sending email: {e}')
+                # Optionally, add error handling here
+    else:
+        form = ContactForm()
+
+    return render(request, 'contact/index.html', {
+        'form': form,
+    })
--- contact_form/contact_form/settings.py
+++ contact_form/contact_form/settings.py
@@ -27,7 +27,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'sowmyam0291@gmail.com'  # Replace with your email
+EMAIL_HOST_PASSWORD = 'wukv jreg crtt fjuz'
 # Application definition
 
 INSTALLED_APPS = [
