77ecd2ac1b9928b5b56c1bfd9f319eee540a0cf1
Sowmya <sowmyam0291@gmail.com>
2024-08-29 11:50:22 +0530

    Test Commit

--- contact_form/contact_form/urls.py
+++ contact_form/contact_form/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+
 from django.urls import path
 from contact.views import index
+
 urlpatterns = [
     path('', index, name='index'),  # Homepage URL
     path('admin/', admin.site.urls),
