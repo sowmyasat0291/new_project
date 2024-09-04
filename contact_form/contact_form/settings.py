45eddbfe3b29adb6871e8792da023b954ecc6b7e
Sowmya <sowmyam0291@gmail.com>
2024-09-04 10:58:47 +0530

    Test Commit

--- contact_form/contact_form/settings.py
+++ contact_form/contact_form/settings.py
@@ -77,12 +77,18 @@ WSGI_APPLICATION = 'contact_form.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Contact_Form',
+        'USER': 'root',
+        'PASSWORD': 'Sowsat@2718',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
     }
 }
 
-
+}
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
